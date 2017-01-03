db0f5d2a899a2e30fd0dd6e302d88719def44263
U-IDEAS\140702 <140702@140702-R30A-IDS.ideas.iii.org.tw>
2017-01-03 18:23:51 +0800

    update title to cut date

--- Youtube-Discourse/youtube_bot.js
+++ Youtube-Discourse/youtube_bot.js
@@ -1,18 +1,20 @@
 var YouTube = require('youtube-node');
-var YAML = require('yamljs')
+var YAML = require('yamljs');
+var request = require('request');
+
 var CHANNEL_ID = 'UCExDf4hkbSU-pmJcyT_sDtg';
 var YOUTUBE_KEY = 'AIzaSyD8F5XTORodGF6CdIVhRLx5mWEtg8w3gPc';
 
-var CATEGORY_ID = '13';//隨意測試區
-var CATEGORY_NAME = 'pdis-site/how-we-work-track';
+//var CATEGORY_ID = '12';//隨意測試區
+//var GET_URI = 'https://talk.pdis.nat.gov.tw/c/12.json';//隨意測試區
+var CATEGORY_ID = '13';//pdis-site/how-we-work-track
+var GET_URI = 'https://talk.pdis.nat.gov.tw/c/6/13.json';//pdis-site/how-we-work-track
 var POST_URI = 'https://talk.pdis.nat.gov.tw/posts';
-var GET_URI = 'https://talk.pdis.nat.gov.tw/c/'+CATEGORY_NAME+'.json';
 var TOPIC_URI = 'https://talk.pdis.nat.gov.tw/t/';
 var YOUTUBE_URI = "https://www.youtube.com/watch?v=";
-var TAGNAME = 'youtube';
+var TAGNAME = 'Youtube';
 var API_KEY = '7af653838dfd6e9f1ed9b25bb447f298b5e855b48dbf22ead2abf5bd74d19576';
 var API_NAME = 'youtube2discourse';
-var request = require('request');
 
 //Set the headers
 var headers = {
@@ -37,11 +39,16 @@ request(opts, function (error, response, results) {
         //console.log(res);
 		for (var i = 0; i < res.topic_list.topics.length; i++) {
 			var title = res.topic_list.topics[i].title;
+			var chk = title.lastIndexOf('-');
+			if(chk > -1){
+				title = title.substring(chk+3, title.length);
+			}
 			var id = res.topic_list.topics[i].id;
 			topic_ary.push(title.toUpperCase());
 			topic_obj[title.toUpperCase()] = id;
 			//console.log(res.topic_list.topics[i]);
 		}
+		console.log(topic_obj);
 		postTo();
     }else{
 		console.log('error='+error+' '+response.statusCode);
@@ -85,7 +92,13 @@ function postTo(){
 									var title = result.items[0].snippet.title;
 									var desc = result.items[0].snippet.description;
 									date = date.substring(0, date.indexOf('T'));
-									console.log(id+" "+date+" "+title+" "+desc);
+									var chk = title.lastIndexOf('-');
+									//檢查標題是否含有日期，有就取用標題日期
+									if(chk > -1){
+										date = title.substring(0, title.indexOf(' '));
+										title = title.substring(title.indexOf(' ')+1, title.length);
+									}
+									console.log(id+" "+date+" "+title);
 									//檢查影片是否已存在要介接的討論區，沒有就介接並發文
 									if(!inArray(title.toUpperCase(), topic_ary)){
 										// Configure the request
@@ -95,7 +108,8 @@ function postTo(){
 											headers: headers,
 											form: {
 												'title': title,
-												'raw': setContent(id, title, desc, date),
+												'created_at': date,
+												'raw': setContent(id),
 												'category': CATEGORY_ID,
 												'api_key': API_KEY,
 												'api_username': API_NAME
@@ -103,7 +117,7 @@ function postTo(){
 										}
 										// Start the request
 										request(options, function (error, response, body) {
-											console.log('post new');
+											console.log('post new '+title);
 											if (!error && response.statusCode == 200) {
 												// Print out the response body
 												console.log(body)
@@ -150,7 +164,7 @@ function postTo(){
 													var tmp = {};
 													tmp[TAGNAME] = YOUTUBE_URI+id;
 													raw.content.push(tmp);
-													raw.date = date;
+													//raw.date = date;
 													var yaml = obj2yaml(raw);
 													needUpldae = 1;
 												}
@@ -194,13 +208,11 @@ function postTo(){
 	});
 }
 //產生文章內容
-function setContent(id, title, desc, date){
+function setContent(id){
 	var obj = {};
 	var obj2 = {};
 	var ary = [];
 	
-	obj["title"] = title;
-	obj["date"] = date;
 	obj2["youtube"] = YOUTUBE_URI+id;
 	ary.push(obj2);
 	obj["content"] = ary;
@@ -223,4 +235,10 @@ function obj2yaml(str){
 	var yaml = YAML.stringify(str);
 	yaml = yaml.replace(/[\'\{\}]/g, "");
 	return yaml;
+}
+// parse a date in yyyy-mm-dd format
+function parseDate(input) {
+  var parts = input.split('-');
+  // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(parts[0], parts[1]-1, parts[2]); // Note: months are 0-based
 }
