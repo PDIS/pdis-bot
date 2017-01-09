601814d6c9d36829e830ee0a83efce6031521351
U-IDEAS\140702 <140702@140702-R30A-IDS.ideas.iii.org.tw>
2017-01-09 17:58:26 +0800

    add config

--- Youtube-Discourse/youtube_bot.js
+++ Youtube-Discourse/youtube_bot.js
@@ -1,23 +1,7 @@
 var YouTube = require('youtube-node');
 var YAML = require('yamljs');
 var request = require('request');
-
-var JSON_PATH = '../Postgres-Discourse/';
-var JSON_NAME = 'discourse_posts';
-
-var CHANNEL_ID = 'UCExDf4hkbSU-pmJcyT_sDtg';
-var YOUTUBE_KEY = 'AIzaSyD8F5XTORodGF6CdIVhRLx5mWEtg8w3gPc';
-
-//var CATEGORY_ID = '12';//隨意測試區
-//var GET_URI = 'https://talk.pdis.nat.gov.tw/c/12.json';//隨意測試區
-var CATEGORY_ID = '13';//pdis-site/how-we-work-track
-var GET_URI = 'https://talk.pdis.nat.gov.tw/c/6/13.json';//pdis-site/how-we-work-track
-var POST_URI = 'https://talk.pdis.nat.gov.tw/posts';
-var TOPIC_URI = 'https://talk.pdis.nat.gov.tw/t/';
-var YOUTUBE_URI = "https://www.youtube.com/watch?v=";
-var TAGNAME = 'Youtube';
-var API_KEY = '7af653838dfd6e9f1ed9b25bb447f298b5e855b48dbf22ead2abf5bd74d19576';
-var API_NAME = 'youtube2discourse';
+var config = require('./config');
 
 //Set the headers
 var headers = {
@@ -30,7 +14,7 @@ var topic_obj = [];
 //取得討論分類內已有的文章列表
 getTopic();
 function getTopic(){
-	var obj = require(JSON_PATH+JSON_NAME);
+	var obj = require(config.JSON_PATH+config.JSON_NAME);
 	for(var v in obj[0].object){
 		//console.log(obj[0].object[v].topic_id);
 		//console.log(obj[0].object[v].post_id);
@@ -51,8 +35,8 @@ function getTopic(){
 function postTo(){
 	//取得頻道內所有的影片列表
 	var youTube = new YouTube();
-	youTube.setKey(YOUTUBE_KEY);
-	youTube.getChannelById(CHANNEL_ID, function(error, results) {
+	youTube.setKey(config.YOUTUBE_KEY);
+	youTube.getChannelById(config.CHANNEL_ID, function(error, results) {
 		if (error) {
 			console.log(error);
 		}else {
@@ -63,7 +47,7 @@ function postTo(){
 			var playlistId = item.contentDetails.relatedPlaylists.uploads;
 			for (var i = 0; i < results.items.length; i++) {
 				yt1 = new YouTube();
-				yt1.setKey(YOUTUBE_KEY);
+				yt1.setKey(config.YOUTUBE_KEY);
 				yt1.getPlayListsItemsById(playlistId, function(error, result) {
 					if (error) {
 						console.log(error);
@@ -74,7 +58,7 @@ function postTo(){
 							var playlistItem = result.items[j];
 							var videoId = playlistItem.snippet.resourceId.videoId;
 							yt2 = new YouTube();
-							yt2.setKey(YOUTUBE_KEY);
+							yt2.setKey(config.YOUTUBE_KEY);
 							yt2.getById(videoId, function(error, result) {
 								if (error) {
 									console.log(error);
@@ -96,16 +80,16 @@ function postTo(){
 									if(!inArray(title.toUpperCase(), topic_ary)){
 										// Configure the request
 										var options = {
-											url: POST_URI,
+											url: config.POST_URI,
 											method: 'POST',
 											headers: headers,
 											form: {
 												'title': title,
 												'created_at': date,
 												'raw': setContent(id),
-												'category': CATEGORY_ID,
-												'api_key': API_KEY,
-												'api_username': API_NAME
+												'category': config.CATEGORY_ID,
+												'api_key': config.API_KEY,
+												'api_username': config.API_NAME
 											}
 										}
 										// Start the request
@@ -123,12 +107,12 @@ function postTo(){
 										var tid = topic_obj[title.toUpperCase()];
 										console.log('repeat: '+tid);
 										var options = {
-											url: TOPIC_URI+tid+'.json?include_raw=1',
+											url: config.TOPIC_URI+tid+'.json?include_raw=1',
 											method: 'GET',
 											headers: headers,
 											form: {
-												'api_key': API_KEY,
-												'api_username': API_NAME
+												'api_key': config.API_KEY,
+												'api_username': config.API_NAME
 											}
 										}
 										// Start the request
@@ -143,34 +127,34 @@ function postTo(){
 												console.log("length="+raw.content.length);
 												for(var i=0;i<raw.content.length;i++){
 													for(var v in raw.content[i]){
-														if(v==TAGNAME) hasTag = i;
+														if(v==config.TAGNAME) hasTag = i;
 													}
 												}
 												if(hasTag >=0){
 													if(raw.content[hasTag].Youtube==undefined || raw.content[hasTag].Youtube==""){
-														raw.content[hasTag].Youtube = YOUTUBE_URI+id;
+														raw.content[hasTag].Youtube = config.YOUTUBE_URI+id;
 														//raw.date = date;
 														var yaml = obj2yaml(raw);
 														needUpdate = 1;
 													}
 												}else{
 													var tmp = {};
-													tmp[TAGNAME] = YOUTUBE_URI+id;
+													tmp[config.TAGNAME] = config.YOUTUBE_URI+id;
 													raw.content.push(tmp);
 													//raw.date = date;
 													var yaml = obj2yaml(raw);
 													needUpdate = 1;
 												}
 												if(needUpdate){
-													console.log(POST_URI+'/'+stream_id);
+													console.log(config.POST_URI+'/'+stream_id);
 													var options = {
-													url: POST_URI+'/'+stream_id,
+													url: config.POST_URI+'/'+stream_id,
 													method: 'PUT',
 													headers: headers,
 													form: {
 														'post[raw]': yaml,
-														'api_key': API_KEY,
-														'api_username': API_NAME
+														'api_key': config.API_KEY,
+														'api_username': config.API_NAME
 													}
 												}
 												// Start the request
@@ -206,7 +190,7 @@ function setContent(id){
 	var obj2 = {};
 	var ary = [];
 	
-	obj2[TAGNAME] = YOUTUBE_URI+id;
+	obj2[config.TAGNAME] = config.YOUTUBE_URI+id;
 	ary.push(obj2);
 	obj["content"] = ary;
 	//var json = JSON.stringify(obj);
