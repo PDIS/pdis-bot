df510dd3958ce993456039b6a27e3f9652e52087
U-IDEAS\140702 <140702@140702-R30A-IDS.ideas.iii.org.tw>
2017-01-17 11:12:17 +0800

    取得標題改回接API並翻頁

--- Youtube-Discourse/youtube_bot.js
+++ Youtube-Discourse/youtube_bot.js
@@ -11,8 +11,49 @@ var headers = {
 }
 var topic_ary = [];
 var topic_obj = [];
-
+var page = 0;
+var length = 0;
 //取得討論分類內已有的文章列表
+getTopic();
+function getTopic(){
+	var opts = {
+		url: config.GET_URI+page,
+		method: 'GET',
+		headers: headers,
+		form: {
+			'api_key': config.API_KEY,
+			'api_username': config.API_NAME
+		}
+	}
+	request(opts, function (error, response, results) {
+		if (!error && response.statusCode == 200) {
+			var res = JSON.parse(results);
+			length = res.topic_list.topics.length;
+			//console.log('length='+length);
+			for (var i = 0; i < length; i++) {
+				var title = res.topic_list.topics[i].title;
+				var chk = title.lastIndexOf('-');
+				if(chk > -1){
+					title = title.substring(chk+3, title.length);
+				}
+				var id = res.topic_list.topics[i].id;
+				topic_ary.push(title.toUpperCase());
+				topic_obj[title.toUpperCase()] = id;
+				//console.log(res.topic_list.topics[i]);
+			}
+			//console.log(topic_obj);
+			if(length>0) {
+				page++;
+				getTopic();
+			}else{
+				postTo();
+			}
+		}else{
+			console.log('get topic list error='+error+' '+response.statusCode);
+		}
+	})
+}
+/*
 fs.readFile(config.JSON_PATH+config.JSON_NAME, 'utf8', function(err, file) {
 	if (err) {
 		//console.log('File not Found');
@@ -33,8 +74,9 @@ fs.readFile(config.JSON_PATH+config.JSON_NAME, 'utf8', function(err, file) {
 		topic_obj[title.toUpperCase()] = id;
 	}
 	//console.log(topic_obj);
-	postTo();
+	//postTo();
 });
+*/
 //介接與發文
 function postTo(){
 	//取得頻道內所有的影片列表
@@ -42,7 +84,7 @@ function postTo(){
 	youTube.setKey(config.YOUTUBE_KEY);
 	youTube.getChannelById(config.CHANNEL_ID, function(error, results) {
 		if (error) {
-			//console.log(error);
+			console.log('getChannelById error='+error);
 		}else {
 		//console.log(JSON.stringify(results, null, 2));
 		//取得頻道內所有影片資訊
@@ -54,7 +96,7 @@ function postTo(){
 				yt1.setKey(config.YOUTUBE_KEY);
 				yt1.getPlayListsItemsById(playlistId, function(error, result) {
 					if (error) {
-						//console.log(error);
+						console.log('getPlayListsItemsById error='+error);
 					}else {
 						//console.log(JSON.stringify(result, null, 2));
 						//取得單一影片資訊
@@ -65,7 +107,7 @@ function postTo(){
 							yt2.setKey(config.YOUTUBE_KEY);
 							yt2.getById(videoId, function(error, result) {
 								if (error) {
-									//console.log(error);
+									console.log('getById error='+error);
 								}else {
 									//console.log(JSON.stringify(result, null, 2));
 									var id = result.items[0].id;
@@ -82,7 +124,8 @@ function postTo(){
 									//console.log(id+" "+date+" "+title);
 									//檢查影片是否已存在要介接的討論區，沒有就介接並發文
 									if(!inArray(title.toUpperCase(), topic_ary)){
-										// Configure the request
+										postNew(id, title, date);
+										/*
 										var options = {
 											url: config.POST_URI,
 											method: 'POST',
@@ -106,6 +149,7 @@ function postTo(){
 												//console.log('error='+error+' '+response.statusCode);
 											}
 										})
+										*/
 									}else{
 										//已存在文章檢查是否需要更新
 										var tid = topic_obj[title.toUpperCase()];
@@ -168,12 +212,12 @@ function postTo(){
 														// Print out the response body
 														//console.log(body)
 													}else{
-														//console.log('error='+error+' '+response.statusCode);
+														console.log('update post error='+error+' '+response.statusCode);
 													}
 												})
 												}
 											}else{
-												//console.log('error='+error+' '+response.statusCode);
+												console.log('get topic error='+error+' '+response.statusCode);
 											}
 										})
 									}
@@ -187,6 +231,31 @@ function postTo(){
 		}
 	  }
 	});
+}
+//貼出新文章
+function postNew(id, title, date){
+	//console.log('post new');
+	var options = {
+		url: config.POST_URI,
+		method: 'POST',
+		headers: headers,
+		form: {
+			'title': title,
+			'created_at': date,
+			'raw': setContent(id),
+			'category': config.CATEGORY_ID,
+			'api_key': config.API_KEY,
+			'api_username': config.API_NAME
+		}
+	}
+	// Start the request
+	request(options, function (error, response, body) {
+		//console.log('post new '+title);
+		//console.log(body)
+		if (error && response.statusCode != 200) {
+			console.log('post new error='+error+' '+response.statusCode);
+		}
+	})
 }
 //產生文章內容
 function setContent(id){
