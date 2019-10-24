9b545ceb7301de611edde0ad6692372fab94ff64
簡就可 <joke0111@gmail.com>
2019-10-24 18:42:23 +0800

    Update discupdater.py

--- SayIt-Discourse/discupdater.py
+++ SayIt-Discourse/discupdater.py
@@ -1,16 +1,17 @@
 #! /usr/local/bin/python2.7
-# -*- coding: utf8 -*
+# -*- coding: utf8 -*-
 import codecs
 import commands
 import json
 import re
-import requests # install requests
-#import sys
+import requests
 import urllib2
-import yaml # install pyyaml
+import yaml
 from datetime import datetime, timedelta
-from lxml import etree # install lxml
+from lxml import etree
 
+### configure your env with `set PYTHONIOENCODING=UTF-8` or use below
+#import sys
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 
@@ -34,7 +35,7 @@ def get_exist_article():
         page_num = page_num + 1
     return ret
 
-def get_sayit_title():   
+def get_sayit_title():
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -43,13 +44,13 @@ def get_sayit_title():
            'Connection': 'keep-alive'}
     request = urllib2.Request(config["sayit-url"]+config["sayit-target-path"], headers=hdr)
     response = urllib2.urlopen(request)
-    page = etree.HTML(response.read(), parser=etree.HTMLParser(encoding="utf-8"))   
+    page = etree.HTML(response.read(), parser=etree.HTMLParser(encoding="utf-8"))
     ret = []
     for txt in page.xpath(u"//li/span/a"):
         ### strip out whitespaces
         txt_strip = txt.text.strip()
+        print(txt_strip)
         m = re.search('(^\d{4})(\-)(0?[1-9]|1[012])(\-)(0?[1-9]|[12][0-9]|3[01])(\s)(.*)$', txt_strip)
-        #print 'sayit:', txt_strip
         if bool(m):
             if txt_strip[:6] == '2016-1' or int(txt_strip[:4]) > 2016:
                 ret.append({'date':''.join(m.groups()[0:5]), 'title':m.group(7), 'url': config["sayit-url"] + txt.values()[0]})
@@ -62,15 +63,15 @@ def check_title(sayit, discourse):
         for j in range(len(discourse)):
             if sayit[i]['title'][:20].upper() == discourse[j]['title'][:20].upper():
                 discourse_id = discourse[j]['id']
-        #print discourse_id , sayit[i]['date'], sayit[i]['title']           
-        ret.append({'date':sayit[i]['date'], 'title':sayit[i]['title'], 'url':sayit[i]['url'], 'id':discourse_id}) 
-    return ret           
+        #print discourse_id , sayit[i]['date'], sayit[i]['title']
+        ret.append({'date':sayit[i]['date'], 'title':sayit[i]['title'], 'url':sayit[i]['url'], 'id':discourse_id})
+    return ret
 
 def update_raw(list_data):
     for i in range(len(list_data)):
         if list_data[i]['id'] == 0:
             discourse_create(list_data[i])
-        elif list_data[i]['id'] != 0:  
+        elif list_data[i]['id'] != 0:
             #print str(list_data[i]['id'])
             r = requests.get(config["discourse-url"] + '/t/topic/'+ str(list_data[i]['id']) +'.json')
             real_id = str(r.json()['post_stream']['posts'][0]['id'])
@@ -91,7 +92,7 @@ def update_raw(list_data):
                     # with Youtube, Soundcloud, Slido, Wiselike, Discourse
                     yaml_raw['content'].append({"Transcript":list_data[i]['url']})
                     list_data[i]['raw'] = yaml_raw
-                    discourse_update(list_data[i]) 
+                    discourse_update(list_data[i])
             else:
                 # Add new content
                 yaml_raw["content"] = [{"Transcript":list_data[i]['url']}]
@@ -121,7 +122,7 @@ def discourse_create(data):
         "raw": yaml.safe_dump(raw, encoding='utf-8', allow_unicode=True, default_flow_style=False),
         "category": config["discourse-category-id"],
         "created_at": str(data['date'])+"T00:00:00.000Z +08:00"
-        }    
+        }
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     url = config["discourse-url"] + '/posts?api_key='+config["discourse-api-key"]+'&api_username='+config["discourse-api-username"]
     resp = requests.post(url, data=post_details, allow_redirects=True, verify=False, headers=headers)
@@ -136,7 +137,7 @@ def log(msg):
 if __name__ == '__main__':
     log('===== ACTION =====')
     list_sayit = get_sayit_title()
-    # print(json.dumps(list_sayit, indent=4, sort_keys=True))
+    # print(json.dumps(list_sayit))
     list_discourse = get_exist_article()
     list_for_update = check_title(list_sayit, list_discourse)
     update_raw(list_for_update)
